752b01af56e600385281f7ac628c4ead715a48b1
rainerpons <rainerpons11@gmail.com>
2023-07-16 18:50:49 -0700

    Removed unnecessary assets and updated unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const appTitle = screen.getByTestId('appTitle');
+  expect(appTitle).toBeInTheDocument();
 });
